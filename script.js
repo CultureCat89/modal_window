bee7e7c50de739dfc8cd67acf2453f821e65ae58
Maksim Ukhanov <uxanov.maksim1989@gmail.com>
2024-02-01 21:26:34 +0300

    save changes 01.02.2024 21:26

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ function init() {
   const wrapper = document.querySelector('.wrapper');
   const modal1 = document.querySelector('.modal-1');
   const modal2 = document.querySelector('.modal-2');
+  const submitForm = document.querySelector('.form-1');
   const openModalButton = document.getElementById("openModal");
   const toggleButton = document.getElementById("toggleButton");
   const closeButton = document.getElementById("closeButton");
   const closeButton2 = document.getElementById("closeButton2");
+  const returnButton = document.getElementById("returnButton");
+  const inputName = document.getElementById("name");
   const inputNumber = document.getElementById("phone");
   
   openModalButton.addEventListener('click', openModal);
-  toggleButton.addEventListener("click", toggleModals);
+
   closeButton.addEventListener("click", closeModal);
   closeButton2.addEventListener("click", closeModal);
-  
+  returnButton.addEventListener("click", closeModal);
+
   inputNumber.addEventListener('input', formatPhoneNumber);
   inputNumber.addEventListener('keydown', handleBackspace);
+  toggleButton.addEventListener("click", validateForm);
+  submitForm.addEventListener('submit', submitData);
 
   function openModal() {
     wrapper.classList.add("open");
@@ -22,7 +28,7 @@ function init() {
   }
 
   function toggleModals() {
-    modal1.classList.add("hidden");
+    modal1.classList.toggle("hidden");
     modal2.classList.toggle("hidden");
   }
 
@@ -30,7 +36,13 @@ function init() {
       modal1.classList.add("hidden");
       modal2.classList.add("hidden");
       wrapper.classList.remove("open");
+      inputName.value = '';
+      inputNumber.value = '';
   }
+
+  // function isClickOutsideModal(e) {
+  //   const withinBoundaries = 
+  // }
   
   function formatPhoneNumber() {
     let inputValue = this.value.replace(/\D/g, '');
@@ -38,14 +50,46 @@ function init() {
     
     this.value = formattedValue;
   }
+
+  function validateForm() {
+    if (!inputName.value || !inputNumber.value || inputNumber.value.length < 16) return;
+    toggleModals();
+  }
   
-  function handleBackspace(event) {
-    if (event.key === 'Backspace') {
+  function handleBackspace(e) {
+    if (e.key === 'Backspace') {
       this.value = this.value.slice(0, -1);
-      event.preventDefault();
+      e.preventDefault();
     }
   }
+
+  function submitData(e) {
+    e.preventDefault();
+  }
+
+  /* Реализаця закрытия по кнопке Esc */
+
+  // document.addEventListener("keydown", isEsc);
+  // function isEsc(e) {
+  //   if (e.keyCode == 27) {
+  //     closeModal();
+  //   }
+  // }
+
+  wrapper.addEventListener('click', (e) => {
+    const composedPath = e.composedPath();
+    const checkingArr = [openModalButton, modal1, modal2];
+    // console.log(composedPath.includes(openModalButton));
+
+    if (!checkingArr.some((value) => composedPath.includes(value))) {
+      // console.log('Ок');
+      // debugger;
+      closeModal();
+    } 
+  });
+
 
 };
 
 document.addEventListener("DOMContentLoaded", init);
+let test;
